7b296555d1e9b4a13f4a2344ccf593529c5f7c94
Daniel Felipe Gómez Martínez <dfgomezm@eafit.edu.co>
2020-03-03 17:29:28 -0500

    Se creo la aplicacion y el servicio web

--- proyectointegrador1/urls.py
+++ proyectointegrador1/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from trabajo1 import views
+
+router = routers.DefaultRouter()
+router.register(r'trabajo1', views.MeasureViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
